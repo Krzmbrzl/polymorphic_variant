0885c35b6064bb2774758155e958d95f5306a78d
Robert Adam <dev@robert-adam.de>
2022-09-10 20:43:49 +0200

    Removed code duplication in benchmarks

--- benchmarks/benchmarks.cpp
+++ benchmarks/benchmarks.cpp
@@ -15,27 +15,39 @@
 #include "initializer.hpp"
 
 
-template< typename T > static void BM_visibleInit(benchmark::State &state) {
-	typename initializer< T >::storage_type value = initializer< T >::visibleInit();
+template< typename T, bool visibleInit > void call_virtual_function(benchmark::State &state) {
+	typename initializer< T >::storage_type value = [&]() {
+		if constexpr (visibleInit) {
+			return initializer< T >::visibleInit();
+		} else {
+			return initializer< T >::hiddenInit();
+		}
+	}();
 
 	for (auto _ : state) {
-		benchmark::DoNotOptimize(value->make_noise());
+		if constexpr (std::is_same_v< std::decay_t< T >, std::variant< Dog, Cat > >) {
+			benchmark::DoNotOptimize(std::visit([](auto &&val) { return val.make_noise(); }, value));
+		} else {
+			benchmark::DoNotOptimize(value->make_noise());
+		}
 	}
+}
+
+template< typename T > static void BM_visibleInit(benchmark::State &state) {
+	call_virtual_function< T, true >(state);
 }
 
 BENCHMARK(BM_visibleInit< pv::polymorphic_variant< Animal, Dog, Cat > >);
 BENCHMARK(BM_visibleInit< Animal >);
+BENCHMARK(BM_visibleInit< std::variant< Dog, Cat > >);
 
 template< typename T > static void BM_hiddenInit(benchmark::State &state) {
-	typename initializer< T >::storage_type value = initializer< T >::hiddenInit();
-
-	for (auto _ : state) {
-		benchmark::DoNotOptimize(value->make_noise());
-	}
+	call_virtual_function< T, false >(state);
 }
 
 BENCHMARK(BM_hiddenInit< pv::polymorphic_variant< Animal, Dog, Cat > >);
 BENCHMARK(BM_hiddenInit< Animal >);
+BENCHMARK(BM_hiddenInit< std::variant< Dog, Cat > >);
 
 static void BM_devirtualized(benchmark::State &state) {
 	Dog dog;
@@ -46,32 +58,8 @@ static void BM_devirtualized(benchmark::State &state) {
 	}
 }
 
-BENCHMARK(BM_devirtualized);
 
-static void BM_classicalVariant_visibleInit(benchmark::State &state) {
-	using T                                       = std::variant< Dog, Cat >;
-	typename initializer< T >::storage_type value = initializer< T >::visibleInit();
-
-	for (auto _ : state) {
-		benchmark::DoNotOptimize(std::visit([](auto &&val) { return val.make_noise(); }, value));
-	}
-}
-
-BENCHMARK(BM_classicalVariant_visibleInit);
-
-static void BM_classicalVariant_hiddenInit(benchmark::State &state) {
-	using T                                       = std::variant< Dog, Cat >;
-	typename initializer< T >::storage_type value = initializer< T >::hiddenInit();
-
-	for (auto _ : state) {
-		benchmark::DoNotOptimize(std::visit([](auto &&val) { return val.make_noise(); }, value));
-	}
-}
-
-BENCHMARK(BM_classicalVariant_hiddenInit);
-
-
-template< typename T > static void BM_linearSearch_visibleInit(benchmark::State &state) {
+template< typename T, bool visibleInit > void perform_linear_search(benchmark::State &state) {
 	std::random_device dev;
 	std::mt19937 rng(dev());
 	std::uniform_int_distribution< int > dist(-5, 5);
@@ -80,96 +68,49 @@ template< typename T > static void BM_linearSearch_visibleInit(benchmark::State 
 	vec.reserve(state.range(0));
 
 	for (std::size_t i = 0; i < state.range(0); ++i) {
-		vec.push_back(initializer< T >::visibleInit(dist(rng)));
+		vec.push_back([&]() {
+			if constexpr (visibleInit) {
+				return initializer< T >::visibleInit(dist(rng));
+			} else {
+				return initializer< T >::hiddenInit(dist(rng));
+			}
+		}());
 	}
 
+	std::random_shuffle(vec.begin(), vec.end());
+
 	for (auto _ : state) {
-		benchmark::DoNotOptimize(
-			// We search for an element that can't exist (due to the limits we chose for our RNG above)
-			// to ensure that we iterate over the entire vector and don't stop early
-			std::find_if(vec.begin(), vec.end(),
-						 [](const typename initializer< T >::storage_type &val) { return val->get_member() > 10; }));
+		// We search for an element that can't exist (due to the limits we chose for our RNG above)
+		// to ensure that we iterate over the entire vector and don't stop early
+		if constexpr (std::is_same_v< std::variant< Dog, Cat >, std::decay_t< T > >) {
+			benchmark::DoNotOptimize(
+				std::find_if(vec.begin(), vec.end(), [](const typename initializer< T >::storage_type &val) {
+					return std::visit([](auto &&v) { return v.get_member() > 10; }, val);
+				}));
+		} else {
+			benchmark::DoNotOptimize(
+				std::find_if(vec.begin(), vec.end(), [](const typename initializer< T >::storage_type &val) {
+					return val->get_member() > 10;
+				}));
+		}
 	}
+}
+
+template< typename T > static void BM_linearSearch_visibleInit(benchmark::State &state) {
+	perform_linear_search< T, true >(state);
 }
 
 BENCHMARK(BM_linearSearch_visibleInit< pv::polymorphic_variant< Animal, Dog, Cat > >)->Range(1, std::pow(8, 6));
 BENCHMARK(BM_linearSearch_visibleInit< Animal >)->Range(1, std::pow(8, 6));
+BENCHMARK(BM_linearSearch_visibleInit< std::variant< Dog, Cat > >)->Range(1, std::pow(8, 6));
 
 template< typename T > static void BM_linearSearch_hiddenInit(benchmark::State &state) {
-	std::random_device dev;
-	std::mt19937 rng(dev());
-	std::uniform_int_distribution< int > dist(-5, 5);
-
-	std::vector< typename initializer< T >::storage_type > vec;
-	vec.reserve(state.range(0));
-
-	for (std::size_t i = 0; i < state.range(0); ++i) {
-		vec.push_back(initializer< T >::hiddenInit(dist(rng)));
-	}
-
-	for (auto _ : state) {
-		benchmark::DoNotOptimize(
-			// We search for an element that can't exist (due to the limits we chose for our RNG above)
-			// to ensure that we iterate over the entire vector and don't stop early
-			std::find_if(vec.begin(), vec.end(),
-						 [](const typename initializer< T >::storage_type &val) { return val->get_member() > 10; }));
-	}
+	perform_linear_search< T, false >(state);
 }
 
 BENCHMARK(BM_linearSearch_hiddenInit< pv::polymorphic_variant< Animal, Dog, Cat > >)->Range(1, std::pow(8, 6));
 BENCHMARK(BM_linearSearch_hiddenInit< Animal >)->Range(1, std::pow(8, 6));
-
-static void BM_classicalVariant_linearSearch_visibleInit(benchmark::State &state) {
-	std::random_device dev;
-	std::mt19937 rng(dev());
-	std::uniform_int_distribution< int > dist(-5, 5);
-
-	using T = std::variant< Dog, Cat >;
-
-	std::vector< typename initializer< T >::storage_type > vec;
-	vec.reserve(state.range(0));
-
-	for (std::size_t i = 0; i < state.range(0); ++i) {
-		vec.push_back(initializer< T >::visibleInit(dist(rng)));
-	}
-
-	for (auto _ : state) {
-		benchmark::DoNotOptimize(
-			// We search for an element that can't exist (due to the limits we chose for our RNG above)
-			// to ensure that we iterate over the entire vector and don't stop early
-			std::find_if(vec.begin(), vec.end(), [](const typename initializer< T >::storage_type &val) {
-				return std::visit([](auto &&v) { return v.get_member() > 10; }, val);
-			}));
-	}
-}
-
-BENCHMARK(BM_classicalVariant_linearSearch_visibleInit)->Range(1, std::pow(8, 6));
-
-static void BM_classicalVariant_linearSearch_hiddenInit(benchmark::State &state) {
-	std::random_device dev;
-	std::mt19937 rng(dev());
-	std::uniform_int_distribution< int > dist(-5, 5);
-
-	using T = std::variant< Dog, Cat >;
-
-	std::vector< typename initializer< T >::storage_type > vec;
-	vec.reserve(state.range(0));
-
-	for (std::size_t i = 0; i < state.range(0); ++i) {
-		vec.push_back(initializer< T >::hiddenInit(dist(rng)));
-	}
-
-	for (auto _ : state) {
-		benchmark::DoNotOptimize(
-			// We search for an element that can't exist (due to the limits we chose for our RNG above)
-			// to ensure that we iterate over the entire vector and don't stop early
-			std::find_if(vec.begin(), vec.end(), [](const typename initializer< T >::storage_type &val) {
-				return std::visit([](auto &&v) { return v.get_member() > 10; }, val);
-			}));
-	}
-}
-
-BENCHMARK(BM_classicalVariant_linearSearch_hiddenInit)->Range(1, std::pow(8, 6));
+BENCHMARK(BM_linearSearch_hiddenInit< std::variant< Dog, Cat > >)->Range(1, std::pow(8, 6));
 
 static void BM_linearSearch_devirtualized(benchmark::State &state) {
 	std::random_device dev;
@@ -182,6 +123,8 @@ static void BM_linearSearch_devirtualized(benchmark::State &state) {
 	for (std::size_t i = 0; i < state.range(0); ++i) {
 		vec.push_back(Dog(dist(rng)));
 	}
+
+	std::random_shuffle(vec.begin(), vec.end());
 
 	for (auto _ : state) {
 		benchmark::DoNotOptimize(
