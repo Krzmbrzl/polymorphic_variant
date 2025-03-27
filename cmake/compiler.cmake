b5ed09becde0da2b146db989ba4a8e7301548c74
Robert Adam <dev@robert-adam.de>
2025-03-27 09:54:29 +0100

    Don't use target for compiler flags (can cause issues in exports)

--- cmake/compiler.cmake
+++ cmake/compiler.cmake
@@ -8,21 +8,21 @@ endif()
 option(PV_DISABLE_WARNINGS "Whether to disable all warnings for this project" ${DEFAULT_DISABLE_WARNINGS})
 option(PV_WARNINGS_AS_ERRORS "Whether to treat all warnings as errors" OFF)
 
-set(DESIRED_FEATURES "ENABLE_MOST_WARNINGS")
+function(set_internal_build_flags TARGET)
+	set(DESIRED_FEATURES "ENABLE_MOST_WARNINGS")
 
-if (PV_WARNINGS_AS_ERRORS)
-	list(APPEND DESIRED_FEATURES "ENABLE_WARNINGS_AS_ERRORS")
-endif()
-if (PV_DISABLE_WARNINGS)
-	list(APPEND DESIRED_FEATURES "DISABLE_ALL_WARNINGS")
-endif()
+	if (PV_WARNINGS_AS_ERRORS)
+		list(APPEND DESIRED_FEATURES "ENABLE_WARNINGS_AS_ERRORS")
+	endif()
+	if (PV_DISABLE_WARNINGS)
+		list(APPEND DESIRED_FEATURES "DISABLE_ALL_WARNINGS")
+	endif()
 
-get_compiler_flags(
-	${DESIRED_FEATURES}
-	DISABLE_DEFAULT_FLAGS
-	OUTPUT_VARIABLE COMPILER_FLAGS
-)
+	get_compiler_flags(
+		${DESIRED_FEATURES}
+		DISABLE_DEFAULT_FLAGS
+		OUTPUT_VARIABLE COMPILER_FLAGS
+	)
 
-add_library(polymorphic_variant_compiler_flags INTERFACE)
-
-target_compile_options(polymorphic_variant_compiler_flags INTERFACE $<BUILD_INTERFACE:${COMPILER_FLAGS}>)
+	target_link_libraries(${TARGET} PRIVATE "${COMPILER_FLAGS}")
+endfunction()
